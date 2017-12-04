d1c669addac01c3943ca46871b0809e9d52ae8fd
ThangPham <thangpham.it92@gmail.com>
2017-12-04 23:47:49 +0700

    add register page

--- app/routes/api.js
+++ app/routes/api.js
@@ -4,22 +4,23 @@ var User = require('../models/user');
 module.exports = function(router) {
     // http://localhost:8080/api/users
     router.post('/users', function (req, res) {
+        // create user
+        var user = new User();
+        user.username = req.body.username;
+        user.password = req.body.password;
+        user.email = req.body.email;
+
         if (req.body.username == null || req.body.username == '' ||
             req.body.password == null || req.body.password == '' ||
             req.body.email == null || req.body.email == '') {
-            res.send('Ensure username, eamil, and password were provided.');
+            res.json({success: false, message: 'Ensure username, email, and password were provided.'});
         } else {
-            // create user
-            var user = new User();
-            user.username = req.body.username;
-            user.password = req.body.password;
-            user.email = req.body.email;
             // save user
             user.save(function (err) {
                 if (err) {
-                    res.send('Username or Email already exists!');
+                    res.json({success: false, message: 'Username or Email already exists!'});
                 } else {
-                    res.send('201: USER CREATED');
+                    res.json({success: true, message: 'User created!'});
                 }
             });
         }
